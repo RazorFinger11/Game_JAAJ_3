c7401d940ad15223a988345a666586f569269a25
RazorFinger <pedro.lopes0203@gmail.com>
2019-07-15 02:02:26 -0300

    FIRST COMMIT

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Input
+    float horizontalInput;
+    float verticalInput;
+
+    // Components
+    Rigidbody rb;
+
+    // Movement
+    public float moveSpeed;
+    public float jumpHeight;
+    public float gravity;
+
+    float verticalVelocity;
+    Vector3 movement;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        horizontalInput = GetHorizontalInput();
+        verticalInput = GetVerticalInput();
+        Jump();
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
+        // getting movement value
+        movement = (transform.forward * verticalInput) + (transform.right * horizontalInput);
+        // normalizing movement
+        movement = movement.normalized * moveSpeed;
+        // getting jumps
+        movement.y = verticalVelocity;
+
+        // applying movement
+        rb.AddForce(movement, ForceMode.VelocityChange);
+        rb.velocity = new Vector3(rb.velocity.x, verticalVelocity, rb.velocity.z);
+    }
+
+    void Jump()
+    {
+        if(isGrounded())
+        {
+            if(verticalVelocity <= 0)
+                verticalVelocity = 0;
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                verticalVelocity = jumpHeight;
+            }
+        }
+        else
+        {
+            verticalVelocity -= gravity * Time.deltaTime;
+        }
+    }
+
+
+    #region Getting Player Input
+    float GetHorizontalInput()
+    {
+        float r = 0;
+        r += Input.GetAxis("Horizontal");
+        return Mathf.Clamp(r, -1.0f, 1.0f);
+    }
+
+    float GetVerticalInput()
+    {
+        float r = 0;
+        r += Input.GetAxis("Vertical");
+        return Mathf.Clamp(r, -1.0f, 1.0f);
+
+    }
+    #endregion
+
+    bool isGrounded()
+    {
+        Ray rayCenter = new Ray(transform.position, transform.up);
+        Ray rayLeft = new Ray(new Vector3(transform.position.x + 0, transform.position.y, transform.position.z), -transform.up);
+        Ray rayRight = new Ray(new Vector3(transform.position.x - 0, transform.position.y, transform.position.z), -transform.up);
+        Ray rayUp = new Ray(new Vector3(transform.position.x, transform.position.y, transform.position.z - 0), -transform.up);
+        Ray rayDown = new Ray(new Vector3(transform.position.x, transform.position.y, transform.position.z + 0), -transform.up);
+
+        Ray diagonalUpLeft = new Ray(new Vector3(transform.position.x, transform.position.y, transform.position.z), -transform.up);
+        Ray diagonalUpRight = new Ray(new Vector3(transform.position.x - 0, transform.position.y, transform.position.z + 0), -transform.up);
+        Ray diagonalDownLeft = new Ray(new Vector3(transform.position.x + 0, transform.position.y, transform.position.z - 0), -transform.up);
+        Ray diagonalDownRight = new Ray(new Vector3(transform.position.x - 0, transform.position.y, transform.position.z - 0), -transform.up);
+
+        Ray SmalldiagonalUpLeft = new Ray(new Vector3(transform.position.x + 0, transform.position.y, transform.position.z + 0), -transform.up);
+        Ray SmalldiagonalUpRight = new Ray(new Vector3(transform.position.x - 0, transform.position.y, transform.position.z + 0), -transform.up);
+        Ray SmalldiagonalDownLeft = new Ray(new Vector3(transform.position.x + 0, transform.position.y, transform.position.z - 0), -transform.up);
+        Ray SmalldiagonalDownRight = new Ray(new Vector3(transform.position.x - 0, transform.position.y, transform.position.z - 0), -transform.up);
+
+        RaycastHit hit;
+
+        float rayDistance = 1f;
+
+        // ------------- SIDE RAYCASTS ------------------
+        Debug.DrawLine(transform.position, transform.position + transform.up * rayDistance, Color.green);
+        Debug.DrawLine(new Vector3(transform.position.x + 0, transform.position.y, transform.position.z), new Vector3(transform.position.x + 0, transform.position.y, transform.position.z) - transform.up * rayDistance, Color.green);
+        Debug.DrawLine(new Vector3(transform.position.x - 0, transform.position.y, transform.position.z), new Vector3(transform.position.x - 0, transform.position.y, transform.position.z) - transform.up * rayDistance, Color.green);
+        // --------------------- UP RAYCASTS ------------------
+        Debug.DrawLine(new Vector3(transform.position.x, transform.position.y, transform.position.z + 0), new Vector3(transform.position.x, transform.position.y, transform.position.z + 0) - transform.up * rayDistance, Color.green);
+        Debug.DrawLine(new Vector3(transform.position.x, transform.position.y, transform.position.z - 0), new Vector3(transform.position.x, transform.position.y, transform.position.z - 0) - transform.up * rayDistance, Color.green);
+        // --------------- DIAGONAL RAYCASTS --------------------
+        Debug.DrawLine(new Vector3(transform.position.x + 0, transform.position.y, transform.position.z + 0), new Vector3(transform.position.x + 0, transform.position.y, transform.position.z + 0) - transform.up * rayDistance, Color.green);
+        Debug.DrawLine(new Vector3(transform.position.x - 0, transform.position.y, transform.position.z + 0), new Vector3(transform.position.x - 0, transform.position.y, transform.position.z + 0) - transform.up * rayDistance, Color.green);
+        Debug.DrawLine(new Vector3(transform.position.x + 0, transform.position.y, transform.position.z - 0), new Vector3(transform.position.x + 0, transform.position.y, transform.position.z - 0) - transform.up * rayDistance, Color.green);
+        Debug.DrawLine(new Vector3(transform.position.x - 0, transform.position.y, transform.position.z - 0), new Vector3(transform.position.x - 0, transform.position.y, transform.position.z - 0) - transform.up * rayDistance, Color.green);
+        // ----------------- SMALLER DIAGONAL RAYCASTS ------------
+        Debug.DrawLine(new Vector3(transform.position.x + 0, transform.position.y, transform.position.z + 0), new Vector3(transform.position.x + 0, transform.position.y, transform.position.z + 0) - transform.up * rayDistance, Color.green);
+        Debug.DrawLine(new Vector3(transform.position.x - 0, transform.position.y, transform.position.z + 0), new Vector3(transform.position.x - 0, transform.position.y, transform.position.z + 0) - transform.up * rayDistance, Color.green);
+        Debug.DrawLine(new Vector3(transform.position.x + 0, transform.position.y, transform.position.z - 0), new Vector3(transform.position.x + 0, transform.position.y, transform.position.z - 0) - transform.up * rayDistance, Color.green);
+        Debug.DrawLine(new Vector3(transform.position.x - 0, transform.position.y, transform.position.z - 0), new Vector3(transform.position.x - 0, transform.position.y, transform.position.z - 0) - transform.up * rayDistance, Color.green);
+
+        if (Physics.Raycast(rayCenter, out hit, rayDistance) || Physics.Raycast(rayLeft, out hit, rayDistance) || Physics.Raycast(rayRight, out hit, rayDistance) || Physics.Raycast(rayUp, out hit, rayDistance) || Physics.Raycast(rayDown, out hit, rayDistance)
+             || Physics.Raycast(diagonalDownLeft, out hit, rayDistance) || Physics.Raycast(diagonalDownRight, out hit, rayDistance) || Physics.Raycast(diagonalUpLeft, out hit, rayDistance) || Physics.Raycast(diagonalUpRight, out hit, rayDistance)
+             || Physics.Raycast(SmalldiagonalDownLeft, out hit, rayDistance) || Physics.Raycast(SmalldiagonalDownRight, out hit, rayDistance) || Physics.Raycast(SmalldiagonalUpLeft, out hit, rayDistance) || Physics.Raycast(SmalldiagonalUpRight, out hit, rayDistance))
+        {
+            return true;
+        }
+        else
+            return false;
+    }
+}
