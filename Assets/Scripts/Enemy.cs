6a8ea8efe02d6dc63e2c17c611126f3fa9e86876
devlog98 <dno1998@hotmail.com>
2019-07-15 18:50:18 -0300

    Imported enemy package

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+    [SerializeField] GameObject spawnPoints;    
+    Transform[] points;
+
+    GameObject model;
+    bool teleported;
+
+    void Start() {
+        var hashPoints = new HashSet<Transform>(spawnPoints.GetComponentsInChildren<Transform>());
+        hashPoints.Remove(spawnPoints.transform);
+        points = hashPoints.ToArray();
+    }
+
+    void Update() {
+        if (!teleported) {
+            this.transform.position = points[Random.Range(0, points.Length)].position;
+            StartCoroutine("WaitToTeleport");
+        }
+    }
+
+    IEnumerator WaitToTeleport() {
+        teleported = true;
+        yield return new WaitForSeconds(1);        
+        teleported = false;
+    }
+}
