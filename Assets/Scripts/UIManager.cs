ea774b0e8f7a8867319cf7157eedfab6d19f68c6
devlog98 <dno1998@hotmail.com>
2019-07-16 14:28:51 -0300

    UI Scalable and Timer

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIManager : MonoBehaviour {
+    public PlayerController playerScript;
+
+    public TextMeshProUGUI healthUI;
+    public TextMeshProUGUI fuelUI;
+    public TextMeshProUGUI timerUI;
+
+    [SerializeField] GameObject victoryPanel;
+    [SerializeField] GameObject defeatPanel;
+
+    bool gameFinished;
+
+    void Update() {
+        if (!gameFinished) {
+            if (Match.gameFinished) {
+                if (playerScript.CurHealth <= 0) {
+                    victoryPanel.SetActive(true);
+                }
+                else {
+                    defeatPanel.SetActive(true);
+                }
+
+                gameFinished = true;
+            }
+
+            healthUI.text = playerScript.CurHealth + "/" + playerScript.maxHealth;
+            fuelUI.text = playerScript.Fuel.ToString();
+            timerUI.text = Match.currentMatchTime.ToString();
+        }
+    }
+}
