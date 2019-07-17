12227d6aa82b07194738b8ea3887f7106e8c50a3
devlog98 <dno1998@hotmail.com>
2019-07-17 18:12:23 -0300

    Necromancer attack anim

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour {
+    #region Attributes
     [SerializeField] GameObject target;
 
     //this game object is just empty, its children are the actual points of teleport
@@ -17,22 +18,30 @@ public class Enemy : MonoBehaviour {
     [Range(0, 10)] [SerializeField] float maxAttackTime = 10f;
 
     [SerializeField] Bullet bulletPrefab;
+    [SerializeField] GameObject firePoint;
     float timeToFire;
-    [SerializeField] float fireRate = 1;
+    [Range(0, 3)] [SerializeField] float minFireRate = .5f;
+    [Range(0, 3)] [SerializeField] float maxFireRate = 1;
+
+    Animator anim;
 
     bool attacking;
+    #endregion
 
     void Start() {
         var hashPoints = new HashSet<Transform>(spawnPoints.GetComponentsInChildren<Transform>());
         hashPoints.Remove(spawnPoints.transform);
         points = hashPoints.ToArray();
+
+        anim = GetComponent<Animator>();
     }
 
-    void Update() {
-        //teleport enemy based on random value between min and max active time
+    void Update() {        
         if (!attacking) {
+            //check if point is spawnable
             Transform[] viablePoints = CheckViablePoints();
 
+            //teleport after attack time(random value between min and max attack time)
             if (viablePoints.Length > 0) {
                 this.transform.position = viablePoints[Random.Range(0, viablePoints.Length)].position;
                 attacking = true;
@@ -40,13 +49,14 @@ public class Enemy : MonoBehaviour {
             }
         }
         else {
-            //look at player and shoot life at certain firerate
+            //look at player
             this.transform.LookAt(target.transform);
+            firePoint.transform.LookAt(target.transform);
 
+            //begin shoot anim (that will trigger shoot event)
             if (Time.time > timeToFire) {
-                timeToFire = Time.time + 1 / fireRate;
-
-                Bullet bullet = Instantiate(bulletPrefab, this.transform.position, this.transform.rotation);
+                anim.SetTrigger("Shoot");
+                timeToFire = (Time.time + anim.GetCurrentAnimatorStateInfo(0).length) + 1 / Random.Range(minFireRate, maxFireRate);                
             }
         }
     }
@@ -63,15 +73,12 @@ public class Enemy : MonoBehaviour {
         return viablePoints.ToArray();
     }
 
+    public void Shoot() {
+        Bullet bullet = Instantiate(bulletPrefab, firePoint.transform.position, firePoint.transform.rotation);
+    }
+
     IEnumerator WaitForAttack(float time) {
         yield return new WaitForSeconds(time);
         attacking = false;
     }
-
-    //void OnDrawGizmos() {
-    //    foreach (Transform point in points) {
-    //        Gizmos.color = Color.green;
-    //        Gizmos.DrawWireSphere(point.position, .5f);
-    //    }
-    //}
 }
