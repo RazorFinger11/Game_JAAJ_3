a95735f440cc4d930548c0a0d0cd277960b3e2c9
RazorFinger <pedro.lopes0203@gmail.com>
2019-07-17 23:57:32 -0300

    game balancing

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour {
     int damage = 10;
+
+    public int maxSpd;
+    public int minSpd;
+
     int speed = 30;
 
     void Update() {
+        speed = Random.Range(minSpd, maxSpd);
         transform.position += this.transform.TransformDirection(Vector3.forward) * Time.deltaTime * speed;
     }
 
     void OnTriggerEnter(Collider other) {
         if (other.gameObject.tag == "Player") {
             other.GetComponent<PlayerController>().Damage(-damage);
+            Destroy(this.gameObject);
         }
     }
 }
