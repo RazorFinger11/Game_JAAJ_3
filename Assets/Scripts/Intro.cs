53ef9de0c4a9e183d53564d4dcc3e91d382fb8bd
devlog98 <dno1998@hotmail.com>
2019-07-21 23:39:20 -0300

    intro

---
+++ Assets/Scripts/Intro.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class Intro : MonoBehaviour {
+    [SerializeField] VideoPlayer player;
+    [SerializeField] LoadManager loadManager;
+
+    float time;
+
+    void Start() {
+        time = Time.time + 3f;
+    }
+
+    void Update() {
+        if (Input.anyKeyDown || (Time.time > time && !player.isPlaying)) {
+            loadManager.LoadScene("Menu");
+        }
+    }
+}
